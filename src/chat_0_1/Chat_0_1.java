75835dd81f4aae26255f333de1aa0625d72fd5fe
Fo InkL <FoinkL@web.de>
2012-03-22 13:35:44 +0100

    bla

---
+++ src/chat_0_1/Chat_0_1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat_0_1;
+
+/**
+ *
+ * @author simon, josh, falk
+ */
+public class Chat_0_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
