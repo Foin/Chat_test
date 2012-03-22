27fc88f3bade1589762c47ecffd912a3ab7c8431
Fo InkL <FoinkL@web.de>
2012-03-22 13:36:41 +0100

    bla

--- src/chat_0_1/Chat_0_1.java
+++ src/chat_0_1/Chat_0_1.java
@@ -14,6 +14,6 @@ public class Chat_0_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic ere
     }
 }
